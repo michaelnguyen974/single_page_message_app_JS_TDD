c9bdb9af6f795df6b8ece11a2653217ca63e1fcd
Michael Nguyen <michaelnguyen974@gmail.com>
2019-01-17 16:50:50 +0000

    use strict test passes by addition of defining variable in different scope

--- spec/MessageSpec.js
+++ spec/MessageSpec.js
@@ -1,12 +1,14 @@
-// 'use strict';
+'use strict';
 
 describe("Message", () => {
+var message;
+
   beforeEach( () => {
-    message = new Message("test text", new Date);
+   message = new Message("test text", new Date);
   });
 
   it("should be able to hold text", () => {
     expect(message.message_content).toEqual("test text")
   });
-
+  
 });
